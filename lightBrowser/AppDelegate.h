fb9cd2ab7cfd8676f47880b23021d5e9d5f58c7f
Cheney <Cheneykids@cheney.local>
2015-12-27 11:29:41 +0800

    Initial Commit

---
+++ lightBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lightBrowser
+//
+//  Created by Cheney on 12/27/15.
+//  Copyright © 2015 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lightBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lightBrowser
+//
+//  Created by Cheney on 12/27/15.
+//  Copyright © 2015 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
